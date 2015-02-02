30cad0ef9e4f2347eb7eda6eb5ef4ce9e4e4eeb2
nerandell <ankitchandawala@gmail.com>
2015-02-02 15:15:30 +0530

    Adding handling of get all addresses and resolve

--- registryclient.py
+++ registryclient.py
@@ -13,6 +13,8 @@ class RegistryClient:
         self._transport = None
         self._protocol = None
         self._pending_requests = {}
+        self._available_services = {}
+        self._assigned_services = {}
 
     def host(self, app, service, version):
         packet = self._make_host_packet(app, service, version)
@@ -39,11 +41,14 @@ class RegistryClient:
             self._handle_provision_response(packet)
 
     def get_all_addresses(self, full_service_name):
-        # returns a list of all (host, port, node_id) that point to instances of given service
-        # this info comes from the registry after registering
-        pass
+        return self._available_services.get(
+            self._get_full_service_name(full_service_name[0], full_service_name[1], full_service_name[2]))
 
     def resolve(self, app: str, service: str, version: str, entity:str):
+        entity_map = self._assigned_services.get(self._get_full_service_name(app, service, version))
+        return entity_map.get(entity)
+
+    def register(self):
         pass
 
     def _make_host_packet(self, app, service, version):
@@ -61,7 +66,12 @@ class RegistryClient:
     def _handle_provision_response(self, packet):
         params = packet['params']
         future = self._pending_requests.pop(params['request_id'])
+        self._available_services = params['result']
         future.set_result(packet['result'])
+
+    @staticmethod
+    def _get_full_service_name(app, service, version):
+        return "{}/{}/{}".format(app, service, version)
 
     @staticmethod
     def _make_provision_packet(request_id, full_service_names):
