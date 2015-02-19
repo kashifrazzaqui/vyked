7a7ba166d6930d7d85659edbc6f10494eb20711a
Ankit Chandawala <ankitchandawala@gmail.com>
2015-02-19 07:10:52 +0530

    Adding logic to send activated packet

--- registry.py
+++ registry.py
@@ -1,8 +1,8 @@
 from enum import Enum
 import signal
 from functools import partial
+from collections import defaultdict
 import asyncio
-from again.utils import unique_hex
 
 from jsonprotocol import RegistryProtocol
 
@@ -12,8 +12,10 @@ class Registry:
         self._ip = ip
         self._port = port
         self._loop = asyncio.get_event_loop()
-        self._services = {}
+        self._registered_services = defaultdict(list)
+        self._pending_services = defaultdict(list)
         self._service_states = {}
+        self._service_protocols = {}
         self._service_dependencies = {}
 
     def _rfactory(self):
@@ -32,53 +34,47 @@ class Registry:
     def receive(self, packet:dict, registry_protocol:RegistryProtocol):
         request_type = packet['type']
         if request_type == 'register':
-            self._register_service(packet)
-        elif request_type == 'provision':
-            self._handle_provision(packet, registry_protocol)
+            self._register_service(packet, registry_protocol)
 
-    def _register_service(self, packet:dict):
+    def _handle_pending_registrations(self):
+        for service_name, nodes in self._pending_services:
+            depends_on = self._service_dependencies[service_name]
+            should_activate = True
+            for app, service, version in depends_on:
+                if self._registered_services.get(self._get_full_service_name(app, service, version)) is None:
+                    should_activate = False
+                    break
+            for node in nodes:
+                if should_activate:
+                    self._send_activated_packet(node, self._service_protocols[node])
+                    self._pending_services.pop(node)
+
+    def _register_service(self, packet:dict, registry_protocol:RegistryProtocol):
         params = packet['params']
         service_name = self._get_full_service_name(params['app'], params["service"], params['version'])
         dependencies = params['dependencies']
         self._service_states[service_name] = self._ServiceState.REGISTERED
         service_entry = (params['host'], params['port'], params['node_id'])
-        if self._services.get(service_name) is None:
-            self._services[service_name] = [service_entry]
-        else:
-            self._services[service_name].append(service_entry)
+        self._registered_services[service_name].append(service_entry)
+        self._pending_services[service_name].append(params['node_id'])
+        self._service_protocols[params['node_id']] = registry_protocol
         if self._service_dependencies[service_name] is None:
             self._service_dependencies[service_name] = dependencies
+        self._handle_pending_registrations()
+
 
     @staticmethod
     def _get_full_service_name(app:str, service:str, version:str):
         return "{}/{}/{}".format(app, service, version)
 
-    def _handle_provision(self, packet:dict, registry_protocol:RegistryProtocol):
-        params = packet['params']
-        result = {}
-        self._add_dependencies(params['app'], params['service'], params['version'], params['service_names'])
-        for app_name, service_name, version in params['service_names']:
-            key = self._get_full_service_name(app_name, service_name, version)
-            result[key] = self._services.get(key)
-        result_params = {'result': result,
-                         'request_id': params['request_id']}
-        result_packet = {'pid': unique_hex(),
-                         'type': 'provision',
-                         'params': result_params}
-        registry_protocol.send(result_packet)
-
-    def _add_dependencies(self, app, service, version, dependencies):
-        full_service_name = self._get_full_service_name(app, service, version)
-        for dependency in dependencies:
-            if self._service_dependencies.get(full_service_name) is None:
-                self._service_dependencies[full_service_name] = [dependency]
-            else:
-                if dependency not in self._service_dependencies[full_service_name]:
-                    self._service_dependencies[full_service_name].append(dependency)
-
     class _ServiceState(Enum):
         REGISTERED = 'registered'
         ACTIVATED = 'activated'
+
+    def _send_activated_packet(self, node:str, protocol:RegistryProtocol):
+        # Send activated message
+        pass
+
 
 if __name__ == '__main__':
     REGISTRY_HOST = '127.0.0.1'
