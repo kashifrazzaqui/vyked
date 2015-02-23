d79d5c77ca009414142551496d71700451abfb8c
Ankit Chandawala <ankitchandawala@gmail.com>
2015-02-23 14:24:35 +0530

    Setting up example

--- example.py
+++ example.py
@@ -1,4 +1,12 @@
+from bus import Bus
+from registry import Registry
 from services import ServiceHost, ServiceClient, api, publish, request, subscribe
+
+REGISTRY_HOST = '127.0.0.1'
+REGISTRY_PORT = 4500
+
+IDENTITY_HOST = '127.0.0.1'
+IDENTITY_PORT = 4501
 
 class IdentityService(ServiceHost):
     @api
@@ -15,7 +23,24 @@ class IdentityClient(ServiceClient):
     def create(self, user_name, password):
         return locals()
 
-
     @subscribe
     def password_changed(self, user_name):
         pass
+
+
+def setup_registry():
+    registry = Registry(REGISTRY_HOST, REGISTRY_PORT)
+    registry.start()
+
+
+def setup_identity_service():
+    bus = Bus(REGISTRY_HOST, REGISTRY_PORT)
+    bus.start(IDENTITY_HOST, IDENTITY_PORT)
+    identity_service = IdentityService()
+    bus.serve(identity_service)
+    bus.start(IDENTITY_HOST, IDENTITY_PORT)
+
+if __name__ == '__main__':
+    setup_registry()
+    setup_identity_service()
+
