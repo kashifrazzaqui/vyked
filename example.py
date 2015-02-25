0d2ef3d10255b5dd856a2990d860010780ac3869
Kashif Razzaqui <kashif.razzaqui@gmail.com>
2015-02-25 18:07:44 +0530

    added a simple description in example.py

--- example.py
+++ example.py
@@ -3,19 +3,24 @@ from services import ServiceHost, ServiceClient, api, publish, request, subscrib
 class IdentityService(ServiceHost):
     @api
     def create(self, user_name, password, request_id=None, entity=None):
+        """ This method is called by the bus when a client remotely calls this api """
         return True
 
     @publish
     def password_changed(self, user_name):
-        pass
+        """Calling this method causes the password_changed message to be sent on the bus"""
+        return user_name
 
 
 class IdentityClient(ServiceClient):
     @request
     def create(self, user_name, password):
+        """Calling this method remotely invokes this api, must be empty and return locals"""
         return locals()
 
 
     @subscribe
     def password_changed(self, user_name):
+        """This method is called by the bus when a ServiceHost publishes something"""
         pass
+
