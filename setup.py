44904af1103a1e07df0c28bec3500ada3ab9b1d4
Ankit Chandawala <ankitchandawala@gmail.com>
2015-03-16 11:33:14 +0530

    Adding dependencies in setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from distutils.core import setup
+
 setup(name='vyked',
-	version='0.1',
-        author='Kashif Razzaqui',
-        author_email='kashif.razzaqui@gmail.com',
-        url='https://github.com/kashifrazzaqui/vyked',
-	description='A micro-service framework for Python',
-        packages=['vyked']
-	)
+      version='0.1',
+      author='Kashif Razzaqui',
+      author_email='kashif.razzaqui@gmail.com',
+      url='https://github.com/kashifrazzaqui/vyked',
+      description='A micro-service framework for Python',
+      packages=['vyked'], requires=['again', 'aiohttp']
+)
 
