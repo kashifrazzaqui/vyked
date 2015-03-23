4f2a70d58a325a945bc4f40305765abb02b45584
Ankit Chandawala <ankitchandawala@gmail.com>
2015-03-23 12:10:21 +0530

    Adding building block classes in models

--- vyked/model.py
+++ vyked/model.py
@@ -1,4 +1,7 @@
 class Entity():
+    """
+    objects which are defined by their identity
+    """
     def __init__(self, id):
         self._id = id
 
@@ -8,7 +11,30 @@ class Entity():
 
 
 class Value():
-
+    """
+    describe a property of a thing, and they don’t have their own identity or lifecycle
+    """
     @staticmethod
     def create(*args, **kwargs):
         pass
+
+
+class Aggregate():
+    """
+    groups of objects which are treated as a single unit
+    """
+    pass
+
+
+class Factory():
+    """
+    used to encapsulate the creation logic of an object or an aggregate
+    """
+    pass
+
+
+class Repository():
+    """
+    used to fetch entities from the used data storage and save the information of entities to it.
+    """
+    pass
