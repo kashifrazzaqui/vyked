b4ee10124feb4650a68a05d4eb715eba87f44614
Ankit Chandawala <ankitchandawala@gmail.com>
2015-03-25 16:53:11 +0530

    Adding logging utilities

--- vyked/__init__.py
+++ vyked/__init__.py
@@ -1,1 +1,9 @@
-__author__ = 'kashif'
+import logging
+from vyked.utils.log import patch_async_emit
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+handler = patch_async_emit(logging.StreamHandler())
+logger.addHandler(handler)
+
