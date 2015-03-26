708bc713d4557e6fbaa7240ac9c4219e851c7f94
Ankit Chandawala <ankitchandawala@gmail.com>
2015-03-26 13:32:31 +0530

    Adding dependency on jsonstreamer

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(name='vyked',
       author_email='kashif.razzaqui@gmail.com',
       url='https://github.com/kashifrazzaqui/vyked',
       description='A micro-service framework for Python',
-      packages=['vyked', 'vyked.utils'], requires=['again', 'aiohttp'])
+      packages=['vyked', 'vyked.utils'], requires=['again', 'aiohttp', 'jsonstreamer'])
 
