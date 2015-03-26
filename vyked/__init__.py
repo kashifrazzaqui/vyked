6f9c851339890269b9b9c9862f45cf139664d11a
Ankit Chandawala <ankitchandawala@gmail.com>
2015-03-26 13:31:48 +0530

    Adding more imports in __init__.py

--- vyked/__init__.py
+++ vyked/__init__.py
@@ -1,11 +1,13 @@
 from vyked.utils.log import patch_async_emit
 
 __all__ = ['TCPServiceClient', 'TCPApplicationService', 'TCPDomainService', 'TCPInfraService', 'HTTPServiceClient',
-           'HTTPApplicationService', 'HTTPDomainService', 'HTTPInfraService', 'Entity', 'Value', 'Aggregate', 'Factory',
-           'Repository', 'Bus', 'Registry', 'log']
+           'HTTPApplicationService', 'HTTPDomainService', 'HTTPInfraService', 'api', 'request', 'subscribe', 'publish',
+           'message_pub', 'get', 'post', 'head', 'put', 'patch', 'delete', 'options', 'trace', 'Entity', 'Value',
+           'Aggregate', 'Factory', 'Repository', 'Bus', 'Registry', 'log']
 
 from .services import (TCPServiceClient, TCPApplicationService, TCPDomainService, TCPInfraService, HTTPServiceClient,
-                       HTTPApplicationService, HTTPDomainService, HTTPInfraService)
+                       HTTPApplicationService, HTTPDomainService, HTTPInfraService, api, request, subscribe, publish,
+                       message_pub, get, post, head, put, patch, delete, options, trace)
 from .model import (Entity, Value, Aggregate, Factory, Repository)
 from .bus import Bus
 from .registry import Registry
