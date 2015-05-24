06a868bb72aa6b5eb1a71f178958c7a8758a22b7
Kashif Razzaqui <kashif.razzaqui@gmail.com>
2015-05-24 14:16:10 +0530

    add sql pool and cursor boilerplate

--- vyked/__init__.py
+++ vyked/__init__.py
@@ -1,7 +1,7 @@
 __all__ = ['TCPServiceClient', 'TCPApplicationService', 'TCPDomainService', 'TCPInfraService', 'HTTPServiceClient',
            'HTTPApplicationService', 'HTTPDomainService', 'HTTPInfraService', 'api', 'request', 'subscribe', 'publish',
            'message_pub', 'get', 'post', 'head', 'put', 'patch', 'delete', 'options', 'trace', 'Entity', 'Value',
-           'Aggregate', 'Factory', 'Repository', 'Bus', 'Registry', 'log']
+           'Aggregate', 'Factory', 'Repository', 'Bus', 'Registry', 'SQLStore' 'log', 'cursor']
 
 from .services import (TCPServiceClient, TCPApplicationService, TCPDomainService, TCPInfraService, HTTPServiceClient,
                        HTTPApplicationService, HTTPDomainService, HTTPInfraService, api, request, subscribe, publish,
@@ -10,5 +10,6 @@ from .model import (Entity, Value, Aggregate, Factory, Repository)
 from .bus import Bus
 from .registry import Registry
 from .utils import log
+from .sql import SQLStore, cursor
 
 log.setup()
---
+++ vyked/sql.py
@@ -0,0 +1,45 @@
+from asyncio import coroutine
+from functools import wraps
+
+from aiopg import create_pool, Pool, Cursor
+
+class SQLStore:
+    _pool = None
+    _connection_params = {}
+
+    @classmethod
+    def connect(cls, database, user, password, host, port):
+        cls._connection_params['database'] = database
+        cls._connection_params['user'] = user
+        cls._connection_params['password'] = password
+        cls._connection_params['host'] = host
+        cls._connection_params['port'] = port
+
+    @classmethod
+    @coroutine
+    def get_pool(cls) -> Pool:
+        if len(cls._connection_params) < 5:
+            raise ConnectionError('Please call SQLStore.connect before calling this method')
+        if not cls._pool:
+            cls._pool = yield from create_pool(**cls._connection_params)
+        return cls._pool
+
+    @classmethod
+    @coroutine
+    def get_cursor(cls) -> Cursor:
+        pool = yield from cls.get_pool()
+        cursor = yield from pool.cursor()
+        return cursor
+
+
+def cursor(func):
+    """
+    decorator that provides a cursor from a pool and executes the function within its context manager
+    """
+
+    @wraps(func)
+    def wrapper(cls, *args, **kwargs):
+        with (yield from cls.get_cursor()) as c:
+            yield from func(cls, c, *args, **kwargs)
+
+    return wrapper
