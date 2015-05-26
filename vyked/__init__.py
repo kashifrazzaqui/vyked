db925dd19061718e5bd228fd437bd3a495092f50
Ankit Chandawala <ankitchandawala@gmail.com>
2015-05-26 14:19:55 +0530

    FIxing typo in __all__

--- vyked/__init__.py
+++ vyked/__init__.py
@@ -1,7 +1,7 @@
 __all__ = ['TCPServiceClient', 'TCPApplicationService', 'TCPDomainService', 'TCPInfraService', 'HTTPServiceClient',
            'HTTPApplicationService', 'HTTPDomainService', 'HTTPInfraService', 'api', 'request', 'subscribe', 'publish',
            'message_pub', 'get', 'post', 'head', 'put', 'patch', 'delete', 'options', 'trace', 'Entity', 'Value',
-           'Aggregate', 'Factory', 'Repository', 'Bus', 'Registry', 'PostgresStore' 'log', 'cursor']
+           'Aggregate', 'Factory', 'Repository', 'Bus', 'Registry', 'PostgresStore', 'log', 'cursor']
 
 from .services import (TCPServiceClient, TCPApplicationService, TCPDomainService, TCPInfraService, HTTPServiceClient,
                        HTTPApplicationService, HTTPDomainService, HTTPInfraService, api, request, subscribe, publish,
