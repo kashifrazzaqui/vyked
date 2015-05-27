39d5bbc199854f6824d885be04d5c6067090201c
Ankit Chandawala <ankitchandawala@gmail.com>
2015-05-27 12:34:23 +0530

    Bumping up version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='vyked',
-      version='1.2.2',
+      version='1.2.21',
       author='Kashif Razzaqui',
       author_email='kashif.razzaqui@gmail.com',
       url='https://github.com/kashifrazzaqui/vyked',
