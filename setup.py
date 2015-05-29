b2c71e480d55d675fd8e66857e737cf33f914bdd
Ankit Chandawala <ankitchandawala@mgail.com>
2015-05-29 16:28:58 +0530

    Updating version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='vyked',
-      version='1.2.31',
+      version='1.2.32',
       author='Kashif Razzaqui',
       author_email='kashif.razzaqui@gmail.com',
       url='https://github.com/kashifrazzaqui/vyked',
