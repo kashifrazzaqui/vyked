82f37ecfe1ea524d949d075d73fd6ed8ec2ce458
blikenoother <b.like.no.other@gmail.com>
2015-06-25 12:33:18 +0530

    install dependency package while setup

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from distutils.core import setup
 
-setup(name='vyked',
-      version='1.2.47',
-      author='Kashif Razzaqui, Ankit Chandawala',
-      author_email='kashif.razzaqui@gmail.com, ankitchandawala@gmail.com',
-      url='https://github.com/kashifrazzaqui/vyked',
-      description='A micro-service framework for Python',
-      packages=['vyked', 'vyked.utils'], requires=['again', 'aiohttp', 'jsonstreamer', 'setproctitle', 'aiopg'])
-
+setup (
+		name='vyked',
+      	version='1.2.47',
+      	author='Kashif Razzaqui, Ankit Chandawala',
+      	author_email='kashif.razzaqui@gmail.com, ankitchandawala@gmail.com',
+      	url='https://github.com/kashifrazzaqui/vyked',
+      	description='A micro-service framework for Python',
+      	packages=['vyked', 'vyked.utils'],
+      	install_requires=['again', 'aiohttp', 'jsonstreamer', 'setproctitle', 'aiopg']
+	)
