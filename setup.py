36efb64df3262f447fea75044f699a6e160304b4
nerandell <ankitchandawala@gmail.com>
2015-06-30 14:43:58 +0530

    Updating setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup
 
 setup(name='vyked',
-      version='1.2.47',
+      version='1.2.48',
       author='Kashif Razzaqui, Ankit Chandawala',
       author_email='kashif.razzaqui@gmail.com, ankitchandawala@gmail.com',
       url='https://github.com/kashifrazzaqui/vyked',
       description='A micro-service framework for Python',
       packages=['vyked', 'vyked.utils'], install_requires=['again', 'aiohttp', 'jsonstreamer', 'setproctitle', 'aiopg',
-                                                           'asyncio_redis', 'asyncio_retrial'])
+                                                           'asyncio_redis', 'async_retrial'])
