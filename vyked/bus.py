a1cc2ef1cdc788dde4aa0a3996c31f783b38ca5a
nerandell <ankitchandawala@gmail.com>
2015-07-13 09:59:38 +0530

    Moving service setup to host class

--- vyked/bus.py
+++ vyked/bus.py
@@ -56,14 +56,6 @@ class Bus:
         self._ronin = False
         self._registered = False
 
-    @property
-    def ronin(self):
-        return self._ronin
-
-    @ronin.setter
-    def ronin(self, value):
-        self._ronin = value
-
     def require(self, args):
         #NOTE: should be in service not bus
         for each in args:
@@ -157,7 +149,6 @@ class Bus:
         else:
             service_client.process_packet(packet)
 
-
     def _host_factory(self):
         return ServiceHostProtocol(self)
 
@@ -238,45 +229,6 @@ class Bus:
                     self._clear_request_queue()
 
             f.add_done_callback(fun)
-
-    def _create_tcp_service_host(self):
-        if self._tcp_host:
-            host_ip, host_port = self._tcp_host.socket_address
-            host_coro = asyncio.get_event_loop().create_server(self._host_factory, host_ip, host_port)
-            return asyncio.get_event_loop().run_until_complete(host_coro)
-
-    def _verify_service_and_version(self, func):
-        def verified_func(*args, **kwargs):
-            query_dict = args[0].GET
-            if isinstance(self._http_host, HTTPApplicationService):
-                return func(*args, **kwargs)
-            if 'service' in query_dict and 'version' in query_dict:
-                if self._http_host.is_for_me(query_dict['service'], query_dict['version']):
-                    return func(*args, **kwargs)
-                else:
-                    return Response(status=421, body="421 wrongly routed request".encode())
-            else:
-                return Response(status=400, body="400 bad request".encode())
-
-        return verified_func
-
-    def _create_http_service_host(self):
-        if self._http_host:
-            host_ip, host_port = self._http_host.socket_address
-            ssl_context = self._http_host.ssl_context
-            app = Application(loop=asyncio.get_event_loop())
-            for each in self._http_host.__ordered__:
-                fn = getattr(self._http_host, each)
-                if callable(fn) and getattr(fn, 'is_http_method', False):
-                    for path in fn.paths:
-                        app.router.add_route(fn.method, path, self._verify_service_and_version(fn))
-                        if self._http_host.cross_domain_allowed:
-                            app.router.add_route('options', path, self._http_host.preflight_response)
-            fn = getattr(self._http_host, 'pong')
-            app.router.add_route('GET', '/ping', fn)
-            handler = app.make_handler()
-            http_coro = asyncio.get_event_loop().create_server(handler, host_ip, host_port, ssl=ssl_context)
-            return asyncio.get_event_loop().run_until_complete(http_coro)
 
     def _create_pubsub_handler(self, host, port):
         self._pubsub_handler = PubSub(host, port)
@@ -373,21 +325,20 @@ class Host:
     _host_id = None
     _tcp_service = None
     _http_service = None
+    _ronin = True
 
     @classmethod
-    def _set_host_id(cls):
-        from uuid import uuid4
-        cls._host_id = uuid4()
+    def set_ronin(cls, ronin):
+        cls._ronin = ronin
+
+    @classmethod
+    def ronin(cls):
+        return cls._ronin
 
     @classmethod
     def _set_process_name(cls):
         from setproctitle import setproctitle
         setproctitle('{}_{}'.format(cls.name, cls._host_id))
-
-    @classmethod
-    def _set_signal_handlers(cls):
-        asyncio.get_event_loop().add_signal_handler(getattr(signal, 'SIGINT'), partial(cls._stop, 'SIGINT'))
-        asyncio.get_event_loop().add_signal_handler(getattr(signal, 'SIGTERM'), partial(cls._stop, 'SIGTERM'))
 
     @classmethod
     def _stop(cls, signame:str):
@@ -408,11 +359,76 @@ class Host:
             cls._set_host_id()
             cls._set_process_name()
             cls._set_signal_handlers()
+            cls._start_server()
         else:
             _logger.error('No services to host')
 
+    @classmethod
+    def _set_signal_handlers(cls):
+        asyncio.get_event_loop().add_signal_handler(getattr(signal, 'SIGINT'), partial(cls._stop, 'SIGINT'))
+        asyncio.get_event_loop().add_signal_handler(getattr(signal, 'SIGTERM'), partial(cls._stop, 'SIGTERM'))
 
+    @classmethod
+    def _create_tcp_server(cls):
+        if cls._tcp_service:
+            host_ip, host_port = cls._tcp_service.socket_address
+            host_coro = asyncio.get_event_loop().create_server(cls._host_factory, host_ip, host_port)
+            return asyncio.get_event_loop().run_until_complete(host_coro)
 
+    @classmethod
+    def _create_http_server(cls):
+        if cls._http_service:
+            host_ip, host_port = cls._http_service.socket_address
+            ssl_context = cls._http_service.ssl_context
+            app = Application(loop=asyncio.get_event_loop())
+            for each in cls._http_service.__ordered__:
+                fn = getattr(cls._http_service, each)
+                if callable(fn) and getattr(fn, 'is_http_method', False):
+                    for path in fn.paths:
+                        app.router.add_route(fn.method, path, fn)
+                        if cls._http_service.cross_domain_allowed:
+                            app.router.add_route('options', path, cls._http_service.preflight_response)
+            fn = getattr(cls._http_service, 'pong')
+            app.router.add_route('GET', '/ping', fn)
+            handler = app.make_handler()
+            http_coro = asyncio.get_event_loop().create_server(handler, host_ip, host_port, ssl=ssl_context)
+            return asyncio.get_event_loop().run_until_complete(http_coro)
+        
+    @classmethod
+    def _set_host_id(cls):
+        from uuid import uuid4
+        cls._host_id = uuid4()
+
+    @classmethod
+    def _start_server(cls):
+        tcp_server = cls._create_tcp_server()
+        http_server = cls._create_http_server()
+        cls._create_pubsub_handler()
+        if tcp_server:
+            _logger.info('Serving TCP on {}'.format(tcp_server.sockets[0].getsockname()))
+        if http_server:
+            _logger.info('Serving HTTP on {}'.format(http_server.sockets[0].getsockname()))
+        _logger.info("Event loop running forever, press CTRL+c to interrupt.")
+        _logger.info("pid %s: send SIGINT or SIGTERM to exit." % os.getpid())
+
+        try:
+            asyncio.get_event_loop().run_forever()
+        except Exception as e:
+            print(e)
+        finally:
+            if tcp_server:
+                tcp_server.close()
+                asyncio.get_event_loop().run_until_complete(tcp_server.wait_closed())
+
+            if http_server:
+                http_server.close()
+                asyncio.get_event_loop().run_until_complete(http_server.wait_closed())
+
+            asyncio.get_event_loop().close()
+
+    @classmethod
+    def _create_pubsub_handler(cls):
+        asyncio.get_event_loop().run_until_complete(cls.pubsub.connect())
 
 if __name__ == '__main__':
     REGISTRY_HOST = '127.0.0.1'
@@ -423,7 +439,7 @@ if __name__ == '__main__':
     HOST_PORT = 8000
     Host.registry = RegistryClient(REGISTRY_HOST, REGISTRY_PORT)
     Host.pubsub = PubSub(REDIS_HOST, REDIS_PORT)
-    Host.attach_tcp_service(tcpx_service)
+    Host.attach_tcp_service(tcp_service)
     Host.attach_http_service(http_service)
     Host.name = 'SampleService'
     Host.run()
