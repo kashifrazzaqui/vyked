71360adae1b7bf86a0d7c1a2e8a0f5ba56a0b45b
nerandell <ankitchandawala@gmail.com>
2015-07-13 16:51:44 +0530

    Cleaning up doc strings

--- vyked/pinger.py
+++ vyked/pinger.py
@@ -6,7 +6,8 @@ class Pinger:
     Pinger to send ping packets to an endpoint and inform if the timeout has occurred
     """
     def __init__(self, handler, interval, timeout, loop=asyncio.get_event_loop()):
-        """Aysncio based pinger
+        """
+        Aysncio based pinger
         :param handler: Pinger uses it to send a ping and inform when timeout occurs.
                         Must implement send_ping() and on_timeout() methods
         :param int interval: time interval between ping after a pong
@@ -21,13 +22,17 @@ class Pinger:
 
     @asyncio.coroutine
     def send_ping(self):
-        """Sends the ping after the interval specified when initializing"""
+        """
+        Sends the ping after the interval specified when initializing
+        """
         yield from asyncio.sleep(self._interval)
         self._handler.send_ping()
         self._start_timer()
 
     def pong_received(self):
-        """Called when a pong is received. So the timer is cancelled"""
+        """
+        Called when a pong is received. So the timer is cancelled
+        """
         self._timer.cancel()
         self.send_ping()
 
