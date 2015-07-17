a6cfd5e1ceb0345fb00bb5f040a8ee1c2559cd32
Anuvrat Parashar <anuvrat@anuvrat.in>
2015-07-17 18:15:51 +0530

    documentation setup.
      - [x] sphinx setup #29
      - [x] hosted on readthedocs.org
      - [x] changed distutils to setuptools.

---
+++ docs/source/devel.rst
@@ -0,0 +1,2 @@
+Setting up the develpment Environment
+=====================================
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='vyked',
       version='1.2.51',
