789741b4216d740a574caac8a2c5466a6807f6d5
Anuvrat Parashar <anuvrat@anuvrat.in>
2015-07-22 16:38:20 +0530

    dependency management
    - moved dependencies out of setup.py and into requirements dir
    - enabled setup.py to construct install_requires from requirements.txt
    - resolved merge conflict of the bumped up version number.

--- setup.py
+++ setup.py
@@ -1,10 +1,41 @@
+# -*- coding: utf-8 -*-
 from setuptools import setup
+from os import getcwd, path
 
-setup(name='vyked',
-      version='1.2.51',
-      author='Kashif Razzaqui, Ankit Chandawala',
-      author_email='kashif.razzaqui@gmail.com, ankitchandawala@gmail.com',
-      url='https://github.com/kashifrazzaqui/vyked',
-      description='A micro-service framework for Python',
-      packages=['vyked', 'vyked.utils'], install_requires=['again', 'aiohttp', 'jsonstreamer', 'setproctitle', 'aiopg',
-                                                           'asyncio_redis', 'async_retrial'])
+if not path.dirname(__file__):  # setup.py without /path/to/
+    _dirname = getcwd()  # /path/to/
+else:
+    _dirname = path.dirname(path.dirname(__file__))
+
+
+def read(name, default=None, debug=True):
+    try:
+        filename = path.join(_dirname, name)
+        with open(filename) as f:
+            return f.read()
+    except Exception as e:
+        err = "%s: %s" % (type(e), str(e))
+        if debug:
+            print(err)
+        return default
+
+
+def lines(name):
+    txt = read(name)
+    return map(
+        lambda l: l.lstrip().rstrip(),
+        filter(lambda t: not t.startswith('#'), txt.splitlines() if txt else [])
+    )
+
+install_requires = [i for i in lines("requirements/base.txt")]
+
+setup(
+    name='vyked',
+    version='1.2.52',
+    author='Kashif Razzaqui, Ankit Chandawala',
+    author_email='kashif.razzaqui@gmail.com, ankitchandawala@gmail.com',
+    url='https://github.com/kashifrazzaqui/vyked',
+    description='A micro-service framework for Python',
+    packages=['vyked', 'vyked.utils'],
+    install_requires=install_requires
+)
