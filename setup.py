e81e142ddf302b537657006e685bd52baceff7fe
nerandell <ankitchandawala@gmail.com>
2015-07-24 14:23:59 +0530

    Using latest version of aiopg

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(name='vyked',
-      version='1.2.53',
+      version='1.2.54',
       author='Kashif Razzaqui, Ankit Chandawala',
       author_email='kashif.razzaqui@gmail.com, ankitchandawala@gmail.com',
       url='https://github.com/kashifrazzaqui/vyked',
       description='A micro-service framework for Python',
-      packages=['vyked', 'vyked.utils'], install_requires=['again', 'aiohttp', 'jsonstreamer', 'setproctitle', 'aiopg',
+      packages=['vyked', 'vyked.utils'], install_requires=['again', 'aiohttp', 'jsonstreamer', 'setproctitle', 'aiopgx',
                                                            'asyncio_redis', 'async_retrial'])
