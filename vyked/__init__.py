4986d9732a146735c2884d4b3c5d9be0df80fa95
nerandell <ankitchandawala@gmail.com>
2015-07-24 11:50:57 +0530

    Fix init.py in modules

--- vyked/__init__.py
+++ vyked/__init__.py
@@ -1,8 +1,8 @@
-__all__ = ['TCPServiceClient', 'TCPApplicationService', 'TCPDomainService', 'TCPInfraService', 'HTTPServiceClient',
-           'HTTPApplicationService', 'HTTPDomainService', 'HTTPInfraService', 'api', 'request', 'subscribe', 'publish',
-           'get', 'post', 'head', 'put', 'patch', 'delete', 'options', 'trace', 'Entity', 'Value',
-           'Aggregate', 'Factory', 'Repository', 'Bus', 'Registry', 'RequestException']
+__all__ = ['Host', 'TCPServiceClient', 'TCPService', 'HTTPService', 'HTTPServiceClient', 'api', 'request', 'subscribe',
+           'publish', 'get', 'post', 'head', 'put', 'patch', 'delete', 'options', 'trace', 'Entity', 'Value',
+           'Aggregate', 'Factory', 'Repository', 'Registry', 'RequestException']
 
+from .host import Host
 from .services import (TCPService, HTTPService, HTTPServiceClient, TCPServiceClient)
 from .decorators.http import (get, post, head, put, patch, delete, options, trace)
 from .decorators.tcp import (api, request, subscribe, publish)
