c8913000a653217efe6e3877e7db932e4be079e6
nerandell <ankitchandawala@gmail.com>
2015-07-26 13:01:05 +0530

    Adding test module

---
+++ tests/test_services.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+
+from vyked import TCPService
+
+class TestService(TCPService):
+    def __init__(self, port):
+        super().__init__('TestService', 1, host_port=port)
+
+class ServiceTests(TestCase):
+
+    def setUp(self):
+        self._test_service = TestService(4500)
+
+    def test_service_identifier(self):
+        self.assertEquals(self._test_service.name, 'TestService'.lower())
+        self.assertEquals(self._test_service.version, 1)
+
+
