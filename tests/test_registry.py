513d5bc0252df16ef64491176a21a0b223633975
sp1rs <shashank201101193@gmail.com>
2015-07-31 13:07:22 +0530

    Added test fixtures in test registry

--- tests/test_registry.py
+++ tests/test_registry.py
@@ -1,55 +1,35 @@
-from vyked.registry import Registry, Repository
 from unittest import mock
 
 
-def test_register_independent_service():
-    s1 = {'service': 'service1', 'version': '1.0.0', 'vendors': [],
-          'host': '192.168.1.2', 'port': 4002, 'node_id': 'n1', 'type': 'type1'}
+def test_register_independent_service(service1, registry):
 
-    r = Registry(ip='192.168.1.1', port=4001, repository=Repository())
+    registry.register_service(packet={'params': service1}, registry_protocol=mock.Mock(),
+                              host='192.168.1.1', port=2001)
 
-    r.register_service(packet={'params': s1}, registry_protocol=mock.Mock(),
-                       host='192.168.1.1', port=2001)
-
-    assert r._repository.get_pending_services() == []
+    assert registry._repository.get_pending_services() == []
 
 
-def test_register_dependent_service():
-    r = Registry(ip='192.168.1.1', port=4001, repository=Repository())
+def test_register_dependent_service(service1, service2, registry):
 
-    s1 = {'service': 'service1', 'version': '1.0.0', 'vendors': [],
-          'host': '192.168.1.2', 'port': 4002, 'node_id': 'n1', 'type': 'type1'}
+    registry.register_service(packet={'params': service2}, registry_protocol=mock.Mock(),
+                              host='192.168.1.1', port=2001)
 
-    s2 = {'service': 'service2', 'version': '1.0.0',
-          'vendors': [{'service': s1['service'], 'version': s1['version']}],
-          'host': '192.168.1.3', 'port': 4003, 'node_id': 'n2', 'type': 'type1'}
+    assert registry._repository.get_pending_services() != []
 
-    r.register_service(packet={'params': s2}, registry_protocol=mock.Mock(),
-                       host='192.168.1.1', port=2001)
-
-    assert r._repository.get_pending_services() != []
-
-    r.register_service(packet={'params': s1}, registry_protocol=mock.Mock(),
-                       host='192.168.1.1', port=2001)
-    assert r._repository.get_pending_services() == []
+    registry.register_service(packet={'params': service1}, registry_protocol=mock.Mock(),
+                              host='192.168.1.1', port=2001)
+    assert registry._repository.get_pending_services() == []
 
 
-def test_deregister_dependent_service():
-    r = Registry(ip='192.168.1.1', port=4001, repository=Repository())
+def test_deregister_dependent_service(service1, service2, registry):
 
-    s1 = {'service': 'service1', 'version': '1.0.0', 'vendors': [],
-          'host': '192.168.1.2', 'port': 4002, 'node_id': 'n1', 'type': 'type1'}
+    registry.register_service(packet={'params': service2}, registry_protocol=mock.Mock(),
+                              host='192.168.1.1', port=2001)
 
-    s2 = {'service': 'service2', 'version': '1.0.0',
-          'vendors': [{'service': s1['service'], 'version': s1['version']}],
-          'host': '192.168.1.3', 'port': 4003, 'node_id': 'n2', 'type': 'type1'}
+    registry.register_service(packet={'params': service1}, registry_protocol=mock.Mock(),
+                              host='192.168.1.1', port=2001)
 
-    r.register_service(packet={'params': s2}, registry_protocol=mock.Mock(),
-                       host='192.168.1.1', port=2001)
+    assert registry._repository.get_pending_services() == []
 
-    r.register_service(packet={'params': s1}, registry_protocol=mock.Mock(),
-                       host='192.168.1.1', port=2001)
-    assert r._repository.get_pending_services() == []
-
-    r.deregister_service(s1['node_id'])
-    assert r._repository.get_pending_services() != []
+    registry.deregister_service(service1['node_id'])
+    assert registry._repository.get_pending_services() != []
