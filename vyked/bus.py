facb9b79882767b65066eb9bfc20aa6d487fd4aa
nerandell <ankitchandawala@gmail.com>
2015-07-31 13:49:16 +0530

    Fix missing host parameter bug

--- vyked/bus.py
+++ vyked/bus.py
@@ -171,7 +171,7 @@ class Bus:
     def start(self, registry_host: str, registry_port: int, redis_host: str, redis_port: int):
         self._set_process_name()
         host = self._tcp_host if self._tcp_host else self._http_host
-        setup_logging('{}_{}'.format(host.name, self.host.socket_address[1]))
+        setup_logging('{}_{}'.format(host.name, host.socket_address[1]))
         ControlPacket.initialize()
         asyncio.get_event_loop().add_signal_handler(getattr(signal, 'SIGINT'), partial(self._stop, 'SIGINT'))
         asyncio.get_event_loop().add_signal_handler(getattr(signal, 'SIGTERM'), partial(self._stop, 'SIGTERM'))
