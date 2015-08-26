d97924ed680a348de69e8af8112020471609768b
nerandell <ankitchandawala@gmail.com>
2015-08-26 16:35:33 +0530

    Fixing test cases

--- tests/test_registry.py
+++ tests/test_registry.py
@@ -2,29 +2,24 @@ from unittest import mock
 
 
 def test_register_independent_service(service1, registry):
-    registry.register_service(packet={'params': service1}, registry_protocol=mock.Mock(),
-                              host='192.168.1.1', port=2001)
+    registry.register_service(packet={'params': service1}, registry_protocol=mock.Mock())
 
     assert registry._repository.get_pending_services() == []
 
 
 def test_register_dependent_service(service1, service2, registry):
-    registry.register_service(packet={'params': service2}, registry_protocol=mock.Mock(),
-                              host='192.168.1.1', port=2001)
+    registry.register_service(packet={'params': service2}, registry_protocol=mock.Mock())
 
     assert registry._repository.get_pending_services() != []
 
-    registry.register_service(packet={'params': service1}, registry_protocol=mock.Mock(),
-                              host='192.168.1.1', port=2001)
+    registry.register_service(packet={'params': service1}, registry_protocol=mock.Mock())
     assert registry._repository.get_pending_services() == []
 
 
 def test_deregister_dependent_service(service1, service2, registry):
-    registry.register_service(packet={'params': service2}, registry_protocol=mock.Mock(),
-                              host='192.168.1.1', port=2001)
+    registry.register_service(packet={'params': service2}, registry_protocol=mock.Mock())
 
-    registry.register_service(packet={'params': service1}, registry_protocol=mock.Mock(),
-                              host='192.168.1.1', port=2001)
+    registry.register_service(packet={'params': service1}, registry_protocol=mock.Mock())
 
     assert registry._repository.get_pending_services() == []
 
@@ -33,7 +28,6 @@ def test_deregister_dependent_service(service1, service2, registry):
 
 
 def test_get_instances(service1, registry):
-    registry.register_service(packet={'params': service1}, registry_protocol=mock.Mock(),
-                              host='192.168.1.1', port=2001)
+    registry.register_service(packet={'params': service1}, registry_protocol=mock.Mock())
     assert registry._repository.get_instances(service1['service'], service1['version']) == [(
         service1['host'], service1['port'], service1['node_id'], service1['type'])]
