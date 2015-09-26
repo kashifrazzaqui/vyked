f3870a6ea52efd3cd5924aa88be81f88cb91745a
Kashif Razzaqui <kashif.razzaqui@gmail.com>
2015-09-26 10:18:22 +0530

    removed commented code

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,36 +14,6 @@ def service(*args, **kwargs):
     return ServiceFactory(*args, **kwargs)
 
 
-# @pytest.fixture
-# def services():
-#     """
-#     returns a dict of services where
-
-#     a1, a2 are independent
-#     b1, b2 depend on a1 and a2 respectively
-#     c1 depends on both a1 and b1
-#     c2 and c3 depend on both a2 and b2
-
-#     more services to be added for different testcases
-#     """
-#     a1 = ServiceFactory()
-#     a2 = ServiceFactory(service=a1['service'], version='1.0.1')
-
-#     b1 = ServiceFactory(dependencies=dependencies_from_services(a1))
-#     b2 = ServiceFactory(
-# service=b1['service'], version='1.0.1',
-# dependencies=dependencies_from_services(a2))
-
-#     c1 = ServiceFactory(dependencies=dependencies_from_services(a1, b1))
-#     c2 = ServiceFactory(service=c1[
-#                         'service'], version='1.0.1', dependencies=dependencies_from_services(a2, b2))
-
-#     c3 = ServiceFactory(service=c1[
-#                         'service'], version='1.0.1', dependencies=dependencies_from_services(a2, b2))
-
-#     return locals()
-
-
 @pytest.fixture
 def dependencies_from_services(*services):
     return [{'service': service['service'], 'version': service['version']} for service in services]
