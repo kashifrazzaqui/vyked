6a314baa4140e7c9ddb74f90fd7af754e6b493b5
Aman Mathur <aman.mathur@TECH-AMANMATHUR.local>
2015-11-18 11:11:06 +0530

    PEP8 changes :-)

--- vyked/utils/ordered_class_member.py
+++ vyked/utils/ordered_class_member.py
@@ -8,9 +8,9 @@ class OrderedClassMembers(type):
 
     def __new__(self, name, bases, classdict):
         classdict['__ordered__'] = [key for key in classdict.keys() if key not in ('__module__', '__qualname__')]
-        for each in bases: #add keys from base classes also
+        for each in bases:  # add keys from base classes also
             odict = getattr(each, '__ordered__', each.__dict__)
             classdict['__ordered__'] = [key for key in odict if key not in ('__module__', '__qualname__')] + \
-                                       classdict['__ordered__']
+                classdict['__ordered__']
 
         return type.__new__(self, name, bases, classdict)
