3a93e81d1af574385c1c246d1bce8e4da14a9314
him3010 <himanshu.jain1@1mg.com>
2017-03-01 11:24:16 +0530

    raise exception in case of timeout of http api

--- vyked/decorators/http.py
+++ vyked/decorators/http.py
@@ -80,6 +80,7 @@ def get_decorated_fun(method, path, required_params, timeout):
                     status = 'timeout'
                     success = False
                     _logger.exception("HTTP request had a timeout for method %s", func.__name__)
+                    raise e
 
                 except VykedServiceException as e:
                     Stats.http_stats['total_responses'] += 1
