fbe75b388f34f41570defb12780216326c254867
Sunny Singh <sunnysingh@1MG-LPTP-516.local>
2018-03-26 18:04:20 +0530

    config tcp to http true by default

--- vyked/config.py
+++ vyked/config.py
@@ -3,4 +3,4 @@ from .utils.common_utils import json_file_to_dict
 config = json_file_to_dict('config.json')
 
 class CONFIG:
-    Convert_Tcp_To_Http = ( isinstance(config, dict) and 'TCP_TO_HTTP' in config and config['TCP_TO_HTTP'] )
+    Convert_Tcp_To_Http =  config['TCP_TO_HTTP'] if (isinstance(config, dict) and 'TCP_TO_HTTP' in config  ) else True
